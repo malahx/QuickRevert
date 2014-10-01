d29bb78d42d3153610a6e3b2cd710cf0d0a116a7
unknown-user <unknown-user@192.168.1.250>
2014-10-01 16:02:39 +0200

    v1.00

---
+++ QuickRevert.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using KSP;
+using UnityEngine;
+
+namespace QuickRevert {
+	[KSPAddon(KSPAddon.Startup.EditorAny | KSPAddon.Startup.TrackingStation | KSPAddon.Startup.Flight | KSPAddon.Startup.SpaceCentre, false)]
+	public class QuickRevert : MonoBehaviour {
+
+		// Initialiser les variables
+		public const string VERSION = "1.00";
+
+		public static bool enable = true;
+
+		[KSPField(isPersistant = true)]
+		private static GameBackup Save_PostInitState;
+		[KSPField(isPersistant = true)]
+		private static GameBackup Save_PreLaunchState;
+		[KSPField(isPersistant = true)]
+		private static Game Save_FlightStateCache;
+		[KSPField(isPersistant = true)]
+		private static ConfigNode Save_ShipConfig;
+
+		// Préparer les évènements
+		private void Awake() {
+			GameEvents.onFlightReady.Add (OnFlightReady);
+			GameEvents.onLevelWasLoaded.Add (OnLevelWasLoaded);
+		}
+
+		// Activer le revert après un quickload
+		private void OnFlightReady() {
+			if (enable) {
+				if (FlightGlobals.ActiveVessel.situation == Vessel.Situations.PRELAUNCH) {
+					try {
+						Save_FlightStateCache = FlightDriver.FlightStateCache;
+						Save_PostInitState = FlightDriver.PostInitState;
+						Save_PreLaunchState = FlightDriver.PreLaunchState;
+						Save_ShipConfig = ShipConstruction.ShipConfig;
+						print ("QuickRevert" + VERSION + ": Revert saved");
+					} catch {
+						print ("QuickRevert" + VERSION + ": Can't save !");
+					}
+				} else {
+					try {  
+						FlightDriver.FlightStateCache = Save_FlightStateCache;
+						FlightDriver.PostInitState = Save_PostInitState;
+						FlightDriver.PreLaunchState = Save_PreLaunchState;
+						ShipConstruction.ShipConfig = Save_ShipConfig;
+						FlightDriver.CanRevertToPostInit = true;
+						FlightDriver.CanRevertToPrelaunch = true;
+						print ("QuickRevert" + VERSION + ": Revert loaded");
+					} catch {
+						print ("QuickRevert" + VERSION + ": Can't load !");
+					}					
+				}
+			}
+		}
+
+		// Initialiser les variables
+		private void OnLevelWasLoaded(GameScenes gamescenes) {
+			if (enable) {
+				if (gamescenes != GameScenes.FLIGHT) {
+					Save_FlightStateCache = null;
+					Save_PostInitState = null;
+					Save_PreLaunchState = null;
+					Save_ShipConfig = null;
+				}
+			}
+		}
+
+		// Supprimer les évènements
+		private void OnDestroy() {
+			GameEvents.onFlightReady.Remove (OnFlightReady);
+			GameEvents.onLevelWasLoaded.Remove (OnLevelWasLoaded);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+﻿#### QuickRevert
+#### A plugin for Kerbal Space Program 0.24.2
+#### Copyright 2014 Malah
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+
+#### What is it ?
+
+QuickRevert is a very small plugin which add the possibility to revert after a quickload
+
+#### How to uninstall it ?
+
+Delete the QuickRevert folder in your KSP/GameData folder.
+
+#### Changelog
+
+v1.00 - 2014.10.01
+Initial release
+
+#### Planned updates/new features
+
+N/A
+
+#### Thanks !
+
+to Squad for this awesome game.
+
+#### Links
+
+https://github.com/malahx/QuickRevert
